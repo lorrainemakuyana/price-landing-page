9257b477b60307dff297f31a55cf18d7aa8669ae
Lorraine Makuyana <lorrainemakuyana23@gmail.com>
2020-06-26 13:18:09 +0000

    Added onclick event

---
+++ script.js
@@ -0,0 +1,10 @@
+
+
+function buttonClick() {
+    var myText = document.getElementById("buttonClick");
+    if (myText.style.display === "none") {
+      myText.style.display = "block";
+    } else {
+      myText.style.display = "none";
+    }
+}
